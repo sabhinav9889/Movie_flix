103f3a57bff687d992d5b1b46754d1c264509404
Abhinav <sabhinav649@gmail.com>
2022-10-08 22:40:53 +0530

    movie_flix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { movie_lis } from "./data/movie_list";
+import Movie_navbar from './Movie_navbar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Movie_navbar/>
+      <ul className='list_'>{
+         movie_lis.map((mov,index) => (
+           <li>
+            <img src={mov.Image} width='500px' height='500px' alt={mov.name}></img>
+            <li><h1>{mov.name}</h1></li>
+            <li><h1>{mov.Date}</h1></li>
+            <button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">Purchase {mov.price}</button>
+           </li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
---
+++ src/data/movie_list.js
@@ -0,0 +1,38 @@
+export const movie_lis=[
+    {
+        price : '800$',
+      name : 'Black x Widow',
+      Date : '2022',
+      Image : 'https://pbs.twimg.com/media/E1Cab4kUcAUIUy-?format=jpg&name=large',
+    },
+    {
+      price : '200$',
+      name : 'The Mask',
+      Date : '1990',
+      Image : 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-90s-movies-1597846450.jpg?crop=0.321xw:0.641xh;0,0.0769xh&resize=640:*',
+    },
+    {
+        price : '100$',
+      name : 'Top Gun',
+      Date : '2022',
+      Image : 'https://m.media-amazon.com/images/M/MV5BZWYzOGEwNTgtNWU3NS00ZTQ0LWJkODUtMmVhMjIwMjA1ZmQwXkEyXkFqcGdeQXVyMjkwOTAyMDU@._V1_.jpg'
+    },
+    {
+        price : '350$',
+      name : 'E.T',
+      Date : '1980',
+      Image : 'https://cdn.mos.cms.futurecdn.net/zZwnwsjfBWY5aPaZwQJkST.jpg'
+    },
+    {
+        price : '130$',
+      name : 'Sita Ramam',
+      Date : '2022',
+      Image : 'https://m.media-amazon.com/images/M/MV5BN2RjZDJhYzUtOTQ5Yy00OWM3LWE5OTctM2Y0YWVmNzAzODllXkEyXkFqcGdeQXVyMTA3MDk2NDg2._V1_.jpg'
+    },
+    {
+        price : '700$',
+      name : 'Smile',
+      Date : '2022',
+      Image : 'https://upload.wikimedia.org/wikipedia/en/7/7f/Smile_%282022_film%29.jpg'
+    }
+  ]
